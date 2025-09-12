090beda5bc07db5ddf73d2a89695e7131000713b
SvIlina <svitlana.ilina@gmail.com>
2025-09-12 16:10:08 +0200

    RewriteContact

--- tests/contact.api.spec.ts
+++ tests/contact.api.spec.ts
@@ -1,38 +1,27 @@
 import {test, expect, APIResponse } from '@playwright/test'
 import ContactPage from '../pages/contact.page'
 import apiController from '../controller/api.controller'
+import { json } from 'stream/consumers'
 
 test.describe('Contact', () =>{
     let contactPage: ContactPage
     let user1: APIResponse
 
 // eslint-disable-next-line require-await
-test.beforeAll(async ({playwright}) => {
-    fakerApi = await playwright.request.newContext({
-        baseURL: 'https://jsonplaceholder.typicode.com/'
-    })
-    const response = await fakerApi.get('users');
-    const responseBody = await response.json();
-    user1 = responseBody[0]
-
-    const postResponse = await fakerApi.post('/users/1/todos', {
-        data: {
-            "title": "Learn Playwright",
-            "completed": "false"
-        }
-    })
-    const postResponseBody = await postResponse.json()
-    console.log(postResponseBody)
+test.beforeAll(async () => {
+    await apiController.init()
+    await apiController.getUsers()
+    await apiController.createUserToDo()
 })
 
 test('Fill contact and verify success message', async ({page}) => {
     contactPage = new ContactPage(page)
     await contactPage.navigateToContactPage()
     await contactPage.submitForm(
-        user1['name'],
-        user1['email'],
-        user1['phone'],
-        user1['website']
+        apiController.getUsers(),
+        apiController.getUsers(),
+        apiController.getUsers['phone'],
+        apiController.getUsers['website']
         
     )
         
