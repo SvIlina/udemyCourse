79f2e46bfbbd71c5a8570d7f23d47dd7dbd626a4
SvIlina <svitlana.ilina@gmail.com>
2025-09-16 13:26:48 +0200

    apiUITests

--- tests/contact.api.spec.ts
+++ tests/contact.api.spec.ts
@@ -1,30 +1,22 @@
 import {test, expect, APIResponse } from '@playwright/test'
 import ContactPage from '../pages/contact.page'
 import apiController from '../controller/api.controller'
-import { json } from 'stream/consumers'
 
 test.describe('Contact', () =>{
     let contactPage: ContactPage
-    let user1: APIResponse
 
 // eslint-disable-next-line require-await
 test.beforeAll(async () => {
     await apiController.init()
-    await apiController.getUsers()
     await apiController.createUserToDo()
 })
 
 test('Fill contact and verify success message', async ({page}) => {
     contactPage = new ContactPage(page)
     await contactPage.navigateToContactPage()
-    await contactPage.submitForm(
-        apiController.getUsers(),
-        apiController.getUsers(),
-        apiController.getUsers['phone'],
-        apiController.getUsers['website']
-        
-    )
-        
+    const user = await apiController.getUsers()
+    contactPage.submitForm(user.name, user.email, user.phone, user.website)
+
     //add soft assertion
     //await expect.soft(contactPage.textArea).toHaveText("Fail test message")
     await expect (contactPage.successMessageBaner).toHaveText('Thanks for contacting us! We will be in touch with you shortly')
